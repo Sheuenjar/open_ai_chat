e4c1f62eea1edb0d33ea06e6acbb65528186424e
sheuen <sheuenjaramillo@gmail.com>
2023-03-30 16:08:09 -0300

    agregando / al final de url de servidor

--- client/script.js
+++ client/script.js
@@ -85,7 +85,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // fetch data from server -> bot's response
-  const response = await fetch('https://open-ai-chat-tjt5.onrender.com', {
+  const response = await fetch('https://open-ai-chat-tjt5.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
